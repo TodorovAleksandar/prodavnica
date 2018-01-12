d4d0f94603c9aedc312fd0ce911a360df7e4d42b
aleksandar <todor.petrovgrad@yahoo.com>
2018-01-12 19:39:00 +0100

    1 commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { BuyersComponentComponent } from './buyers-component/buyers-component.component';
+import { ProductsComponentComponent } from './products-component/products-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    BuyersComponentComponent,
+    ProductsComponentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
